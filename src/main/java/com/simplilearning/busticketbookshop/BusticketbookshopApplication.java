665ee21eba23e3c9f9a1c8d6f4e82b953bcbcb07
mallela009 <mallelaarjuna009@gmail.com>
2022-11-30 16:37:34 +0530

    first commit

---
+++ src/main/java/com/simplilearning/busticketbookshop/BusticketbookshopApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearning.busticketbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusticketbookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusticketbookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearning/busticketbookshop/BusticketbookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearning.busticketbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusticketbookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
